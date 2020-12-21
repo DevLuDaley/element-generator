18924a5475a6b9e19b702812ccbe754209a1daa4
devludaley <daleyluis@gmail.com>
2020-12-21 18:16:28 -0500

    Update ConverterPage.js

--- src/components/ConverterPage.js
+++ src/components/ConverterPage.js
@@ -1,5 +1,8 @@
 import React, { Fragment, useState, useEffect }from 'react'
 import Elements from '../components/Elements'
+import ElementsData from './ElementsData'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import CopyToClip from './CopyToClip'
 // import PropTypes from 'prop-types'
 // import {setState} from re
 // var [name, setName] = useState()
@@ -19,6 +22,8 @@ import Elements from '../components/Elements'
         
 var [name, setName] = useState("")
 var [confirmed, setConfirmed] = useState(false)
+const [value, setValue] = useState('')
+const [copied, setCopied] = useState(false)
     // const [] = setState([])
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -27,24 +32,27 @@ var [confirmed, setConfirmed] = useState(false)
     }
     return (
         <Fragment> 
-
+            <h1>COVERTER PAGE</h1>
         <div>
               <form id='new-recipe-form' onSubmit={handleSubmit}>
                 <label> Enter text to be converted </label> 
                     <input type="text" placeholder='enter text...' value={name} name="name" onChange={e => setName(e.target.value)}/>
-                        <button type="submit">Convert to Tag</button>
-                           </form>
+                        <button type="submit">Convert to Tag
+                        </button>
+              </form>
+
+                        <button onClick={(e) => {console.log("value of e.target => ", e.target)}}
+                            type="copy">Copy Text to clipboad
+                        </button>
+                            
                            <br>
-                               </br>
-                           { 
-                               confirmed ? 
+                           </br>
+                           { confirmed ? 
                                <section name={name}>
                                &lt;option&gt;
-                               <br>
-                               </br>
+                               <br></br>
                                 { name }
-                               <br>
-                               </br>
+                               <br></br>
                                &lt;/option&gt;
                                </section>
                                :
@@ -52,6 +60,8 @@ var [confirmed, setConfirmed] = useState(false)
                            }
         </div>
         <Elements/>
+        <ElementsData/>
+        <CopyToClip/>
         </Fragment>
     )
 }
