861b5405d2bf4f3b77d52ae00a4cecbbc2fccb59
devludaley <daleyluis@gmail.com>
2020-12-20 20:04:11 -0500

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Fragment, Component } from 'react'
+import '../public/App.css'
+const App = () => {
+    return (
+    <Fragment className="App">
+    <h1> Hello, World!</h1>
+    </Fragment>  );
+}
+export default App;
