b8251888af40c39961b9c23d280dceb74e3794ab
devludaley <daleyluis@gmail.com>
2020-12-22 17:30:47 -0500

    comment out unused hot reload logic until needed

--- src/index.js
+++ src/index.js
@@ -23,6 +23,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// if (module.hot) {
+//    module.hot.accept('../dist/print.js', function() {
+//      console.log('GREAT JOB LU!');
+//      printMe();
+//    })
+//  }
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
