2d86ba0cbed5727788859c58ebf95ffcec936e84
devludaley <daleyluis@gmail.com>
2020-12-23 21:16:43 -0500

    change [key,value] variable to  [k,v]

---
+++ .history/src/components/optionsArray_20201223210616.js
@@ -0,0 +1,59 @@
+import React, {Fragment, useState} from 'react'
+export const optionsArray =
+[
+    {k: "div", value: "<div> </div>"},
+    {k: "span", value: "<span> </span>"},
+    {k: "html", value: "<html> </html>"},
+    {k: "head", value: "<head> </head>"},
+    {k: "body", value: "<body> </body>"},
+    {k: "ul", value: "<ul> </ul>"},
+    {k: "ol", value: "<ol> </ol>"},
+    {k: "lh", value: "<lh> </lh>"},
+    {k: "li", value: "<li> </li>"},
+    {k: "table", value: "<table> </table>"},
+
+    {k: "title", value: "<title> </title>"},
+    {k: "thead", value: "<thead> </thead>"},
+
+    {k: "tbody", value: "<tbody> </tbody>"},
+    {k: "tr", value: "<tr> </tr>"},
+    {k: "th", value: "<th> </th>"},
+    {k: "pre", value: "<pre> </pre>"},
+    {k: "p", value: "<p> </p>"},
+    {k: "nav", value: "<nav> </nav>"},
+    {k: "main", value: "<main> </main>"},
+    {k: "header", value: "<header> </header>"},
+    {k: "h1", value: "<h1> </h1>"},
+    {k: "h2", value: "<h2> </h2>"},
+    {k: "h3", value: "<h3> </h3>"},
+    {k: "h4", value: "<h4> </h4>"},
+    {k: "h5", value: "<h5> </h5>"},
+    {k: "h6", value: "<h6> </h6>"},
+    {k: "footer", value: "<footer> </footer>"},
+    {k: "i", value: "<i> </i>"},
+    {k: "b", value: "<b> </b>"},
+    {k: "u", value: "<u> </u>"},
+    {k: "q", value: "<q> </q>"}
+]
+
+// const firstNames = optionsArray.map( e => e)
+
+// console.log("first names", firstNames)
+// const wordSplit = elementsArray[1].split(" ")
+// const wordSplit = elementsArray //.split(" ")
+// const [startTag, endTag] =  wordSplit
+// console.log("wordSplit", wordSplit)
+// console.log("wordSplit[0]", wordSplit[0]["div"])
+// return (
+//     <Fragment>
+//     <h1>ELEMENTS DATA</h1>
+//         {/* {elementsArray[1]} */}
+//         {/* {wordSplit[0]} text that needs to be divd
+//         {" " + wordSplit[1]} */}
+//         {/* {wordSplit} */}
+//         {/* {startTag + " "}
+//         things
+//         {" " + endTag} */}
+//     </Fragment>  );
+// }
+// export default optionsArray;
